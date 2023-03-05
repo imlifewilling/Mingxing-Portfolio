7fdbc1147dcfacf32d805ac6a01cbce782ff8079
Mingxing Li <mingxingli@lnc-168885.lan>
2023-03-05 14:23:18 -0500

    modified database

--- backend_sandity/schemas/index.js
+++ backend_sandity/schemas/index.js
@@ -1,10 +1,8 @@
-import works from './works';
-import testimonials from './testimonials';
-import brands from './brands';
+import projects from './projects';
 import abouts from './abouts';
 import experiences from './experiences';
 import skills from './skills';
 import workExperience from './workExperience';
 import contact from './contact';
 
-export const schemaTypes = [works, testimonials, brands, abouts, skills, workExperience, experiences, contact];
+export const schemaTypes = [projects, abouts, skills, workExperience, experiences, contact];
